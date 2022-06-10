e199ffc4eb0ff9430fb12fefbd9490c33bc3ab05
Diener Dornelas <diener.ld@outlook.com>
2022-06-09 22:59:56 -0300

    feature: add modal for register a new user

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 const elementValidUsername = document.querySelector<HTMLSpanElement>("#errorUsername")!;
 const elementValidPassword = document.querySelector<HTMLSpanElement>("#errorPassword")!;
+const formRegister = document.querySelector('#form-register-user')!;
 
+
+// TODO: Fazer Validação de campos
 
 const session = localStorage.getItem('session');
 let logged = sessionStorage.getItem('logged');
-
+type User = {
+  username: string,
+  password: string,
+  messages: string[]
+}
 
 function saveSession(data: string) {
   sessionStorage.setItem('logged', data);
@@ -22,8 +29,8 @@ function checkLogged() {
   }
 }
 
-function alert() {
-  const alertLogin = document.querySelector<HTMLDivElement>('#alertLogin')!;
+function alert(idAlert: string) {
+  const alertLogin = document.querySelector<HTMLDivElement>(`#${idAlert}`)!;
   alertLogin.classList.remove('d-none');
   return;
 }
@@ -45,9 +52,9 @@ function togglePassword() {
   return;
 }
 
-function validateUsername() {
+function validateInput(id: string,) {
   let usernameValid = false;
-  const username = document.querySelector<HTMLInputElement>("#userInputUsername")!.value as string;
+  const username = document.querySelector<HTMLInputElement>(`#${id}`)!.value as string;
   if (username.length < 3) {
     elementValidUsername.classList.toggle('d-none', usernameValid);
 
@@ -58,43 +65,53 @@ function validateUsername() {
   return;
 }
 
-function validatePassword() {
-  let passwordValid = false;
-  const password = document.querySelector<HTMLInputElement>("#userInputPassword")!.value as string;
-
-  if (password.length < 3) {
-    elementValidPassword.classList.toggle('d-none', passwordValid);
-    return;
-  }
-  passwordValid = !passwordValid;
-  elementValidPassword.classList.toggle('d-none', passwordValid);
-
-  return;
-}
 
 
-function submitForm() {
+
+function submitFormLogin() {
   const username = document.querySelector<HTMLInputElement>("#userInputUsername")!.value as string;
   const password = document.querySelector<HTMLInputElement>("#userInputPassword")!.value as string;
 
   if (!elementValidPassword.classList.contains('d-none')
     || !elementValidUsername.classList.contains('d-none')) {
-    validatePassword();
-    validateUsername();
+    validateInput('userInputUsername');
+    validateInput('userInputPassword');
+
     return;
   }
+
   if (!username || !password) {
-    validatePassword();
-    validateUsername();
+    validateInput('userInputUsername');
+    validateInput('userInputPassword');
+
     return;
   }
 
   const user = JSON.parse(localStorage.getItem(username) || '{}');
   if (!(password === user.password) || !user) {
-    alert();
+    alert('alertLogin');
     return;
   }
   saveSession(username);
-
   window.location.href = './home.html';
+
+  return;
 }
+
+function submitFormRegister(e: Event) {
+  e.preventDefault();
+  const username = document.querySelector<HTMLInputElement>("#username-register-input")!.value as string;
+  const password = document.querySelector<HTMLInputElement>("#password-register-input")!.value as string;
+  const passwordRepeat = document.querySelector<HTMLInputElement>("#password-repeat-register-input")!.value as string;
+
+  if (!username || !password || !passwordRepeat) {
+    alert('alertRegister');
+    document.getElementById('modal-close')!.click();
+    return;
+  }
+
+}
+
+formRegister.addEventListener('submit', submitFormRegister);
+
+checkLogged();
