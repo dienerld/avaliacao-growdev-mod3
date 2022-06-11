5113b86de1f9d3511e29df0c4ac34f2de01501ac
Diener Dornelas <diener.ld@outlook.com>
2022-06-10 21:22:54 -0300

    refactor and finish index page

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-const elementValidUsername = document.querySelector<HTMLSpanElement>("#errorUsername")!;
-const elementValidPassword = document.querySelector<HTMLSpanElement>("#errorPassword")!;
 const formRegister = document.querySelector('#form-register-user')!;
 
 
@@ -7,14 +5,24 @@ const formRegister = document.querySelector('#form-register-user')!;
 
 const session = localStorage.getItem('session');
 let logged = sessionStorage.getItem('logged');
-type User = {
-  username: string,
-  password: string,
-  messages: string[]
+type TMessage = {
+  description: string,
+  details: string,
 }
 
-function saveSession(data: string) {
-  sessionStorage.setItem('logged', data);
+
+type TUser = {
+  username: string,
+  password: string,
+  messages: TMessage[]
+}
+
+// Utils
+function saveSession(username: string, stayConnected: boolean = false) {
+  if (stayConnected) {
+    localStorage.setItem('session', username);
+  }
+  sessionStorage.setItem('logged', username);
 }
 
 function checkLogged() {
@@ -35,8 +43,8 @@ function alert(idAlert: string) {
   return;
 }
 
-function togglePassword() {
-  const elementInputPassword = document.querySelector('#userInputPassword') as HTMLInputElement;
+function togglePasswordLogin() {
+  const elementInputPassword = document.querySelector('#user-input-password') as HTMLInputElement;
   const elementIconEye = document.querySelector('#iconEye') as HTMLElement;
 
   if (!elementInputPassword) return;
@@ -52,37 +60,63 @@ function togglePassword() {
   return;
 }
 
-function validateInput(id: string,) {
+function validateRegisterUsername() {
   let usernameValid = false;
-  const username = document.querySelector<HTMLInputElement>(`#${id}`)!.value as string;
-  if (username.length < 3) {
-    elementValidUsername.classList.toggle('d-none', usernameValid);
+  const inputUsername = document.querySelector<HTMLInputElement>('#username-register-input')!;
+  const spanError = document.querySelector<HTMLInputElement>('#error-username')!;
+  if (inputUsername.value.length < 3) {
+    spanError.classList.toggle('d-none', usernameValid);
 
     return;
   }
   usernameValid = !usernameValid;
-  elementValidUsername.classList.toggle('d-none', usernameValid);
+  spanError.classList.toggle('d-none', usernameValid);
   return;
 }
 
+function validateRegisterPassword() {
+  let passwordValid = false;
+  const inputUsername = document.querySelector<HTMLInputElement>('#password-register-input')!;
+  const spanError = document.querySelector<HTMLInputElement>('#error-password')!;
+  if (inputUsername.value.length < 4) {
+    spanError.classList.toggle('d-none', passwordValid);
 
+    return;
+  }
+  passwordValid = !passwordValid;
+  spanError.classList.toggle('d-none', passwordValid);
+  return;
+}
 
+function validateMatchPassword() {
+  let passwordValid = false;
+  const spanError = document.querySelector<HTMLInputElement>('#error-repeat-password')!;
+  const password = document.querySelector<HTMLInputElement>("#password-register-input")!.value as string;
+  const passwordRepeat = document.querySelector<HTMLInputElement>("#password-repeat-register-input")!.value as string;
 
-function submitFormLogin() {
-  const username = document.querySelector<HTMLInputElement>("#userInputUsername")!.value as string;
-  const password = document.querySelector<HTMLInputElement>("#userInputPassword")!.value as string;
-
-  if (!elementValidPassword.classList.contains('d-none')
-    || !elementValidUsername.classList.contains('d-none')) {
-    validateInput('userInputUsername');
-    validateInput('userInputPassword');
+  if (password !== passwordRepeat) {
+    spanError.classList.toggle('d-none', passwordValid);
 
     return;
   }
 
+  passwordValid = !passwordValid;
+  spanError.classList.toggle('d-none', passwordValid);
+
+  return;
+}
+
+// Forms
+function submitFormLogin() {
+  const form = document.querySelector<HTMLFormElement>("#form-login")!;
+  const username = form.username.value;
+  const password = form.password.value;
+  const stayConnected = document.querySelector<HTMLInputElement>('#session-check')!.checked;
+
+
+
   if (!username || !password) {
-    validateInput('userInputUsername');
-    validateInput('userInputPassword');
+    alert("alertLogin")
 
     return;
   }
@@ -92,7 +126,9 @@ function submitFormLogin() {
     alert('alertLogin');
     return;
   }
-  saveSession(username);
+
+
+  saveSession(username, stayConnected);
   window.location.href = './home.html';
 
   return;
@@ -100,18 +136,39 @@ function submitFormLogin() {
 
 function submitFormRegister(e: Event) {
   e.preventDefault();
-  const username = document.querySelector<HTMLInputElement>("#username-register-input")!.value as string;
-  const password = document.querySelector<HTMLInputElement>("#password-register-input")!.value as string;
-  const passwordRepeat = document.querySelector<HTMLInputElement>("#password-repeat-register-input")!.value as string;
+  const form = document.querySelector<HTMLFormElement>("#form-register-user")!;
+  const username = form.username.value;
+  const password = form.password.value;
+  const passwordRepeat = form["password-repeat"].value;
 
   if (!username || !password || !passwordRepeat) {
     alert('alertRegister');
-    document.getElementById('modal-close')!.click();
     return;
   }
 
+  if (password !== passwordRepeat) return;
+
+  if (window.localStorage.getItem(username)) {
+    alert('alertRegister');
+    return;
+  }
+
+  const user: TUser = {
+    username,
+    password,
+    messages: []
+  }
+
+  window.localStorage.setItem(username, JSON.stringify(user));
+  saveSession(username);
+
+  window.location.href = './home.html';
+
 }
 
+// Start
 formRegister.addEventListener('submit', submitFormRegister);
-
 checkLogged();
+
+// Only remove erros tsc
+export { }
